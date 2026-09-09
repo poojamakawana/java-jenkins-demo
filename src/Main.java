d9506006a3fa2a799192b4d64c63ac291450afc5
Pooja makawana <poojamakawana.it@ddu.ac.in>
2026-09-09 16:23:15 +0530

    initial java jenkins project

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello from Jenkins!");
+        System.out.println("This Java application is running inside Docker.");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
