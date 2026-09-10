823dba7d1bfa5558ea23a80990e9ed7f93b90d24
Pooja makawana <poojamakawana.it@ddu.ac.in>
2026-09-10 14:23:21 +0530

    Add Docker Hub push securely

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello from Jenkins!");
+        System.out.println("Hello from Jenkins!version 2");
         System.out.println("This Java application is running inside Docker.");
 
         for (int i = 1; i <= 5; i++) {
